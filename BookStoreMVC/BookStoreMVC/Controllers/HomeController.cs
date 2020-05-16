f1b7db44b391ea333fa905e2dd542e8976b37c08
Thien Ngan <nguyendacthienngan@gmail.com>
2020-05-16 23:11:15 +0700

    Update Project UI bookstore

--- BookStoreMVC/BookStoreMVC/Controllers/HomeController.cs
+++ BookStoreMVC/BookStoreMVC/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BookStoreMVC.Data;
+using BookStoreMVC.Models;
 namespace BookStoreMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private BookContext db = new BookContext();
         public ActionResult Index()
         {
             return View();
